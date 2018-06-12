54b47c3acbd5a200daeec111b0d4fafcbe7197c5
StevenCuasqui <cuasquisteven@gmail.com>
2018-06-12 17:17:54 -0500

    correccion

--- navbar.php
+++ navbar.php
@@ -15,16 +15,6 @@
             <?php
                 if ( $_SESSION["userType"] == 'prof' ) {
             ?>
-            <li2 class="nav-item" data-toggle="tooltip" data-placement="right" title="Crear">
-                <a class="nav-link"><p>
-                    <span class="nav-link-text">(?) Pantalla de Profesor: En esta pagina se muestra las opciones para profesor para:</span></p>
-                    <span class="nav-link-text">- Crear: Crea un OA con ExeLearning.</span>
-                  </br>
-                    <span class="nav-link-text">- importar: Sube un OA en formato .zip creado con ExeLearning al repositorio de OA.</span>
-                  </br>
-                    <span class="nav-link-text">- Buscar: Busca un OA en el repositorio de OA.</span>
-                </a>
-            </li2>
                 <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Crear">
                     <a class="nav-link" href="exe.php">
                         <i class="fa fa-fw fa-file-text-o"></i>
@@ -48,12 +38,8 @@
                     <li>
                         <a href="buscar.php">Buscar Objetos de Aprendizaje</a>
                     </li>
-
-
                 </ul>
             </li>
-
-
             <?php
                 if ( $_SESSION["userType"] != 'admin' ) {
             ?>
@@ -63,7 +49,6 @@
                         <span class="nav-link-text">Herramientas Adicionales</span>
                     </a>
                 </li>
-
             <?php } ?>
             <?php
                 if ( $_SESSION["userType"] == 'admin' ) {
@@ -74,30 +59,7 @@
                         <span class="nav-link-text">Usuarios</span>
                     </a>
                 </li>
-                <li2 class="nav-item" data-toggle="tooltip" data-placement="right" title="Crear">
-                    <a class="nav-link">
-                      <p>
-                        <span class="nav-link-text">(?) Pantalla de Administrador: En esta pagina se muestran las opciones del administrador para el manejo de Usuarios</span>
-                      </p>
-                      <span class="nav-link-text">- El administrador puede además de buscar un OA en el repositorio, elimiar estu y profesores.</span>
-                    </a>
-                </li2>
-
             <?php } ?>
-
-            <?php
-                if ( $_SESSION["userType"] != 'admin' && $_SESSION["userType"] != 'prof' ) {
-            ?>
-                <li2 class="nav-item" data-toggle="tooltip" data-placement="right" title="Crear">
-                    <a class="nav-link">
-                      <p>
-                        <span class="nav-link-text">(?) Pantalla de Estudiante: En esta pagina se muestran las opciones del estudiante.</span>
-                      </p>
-                      <span class="nav-link-text">- Buscar: busca los OA que se encuentren en el repositorio.</span>
-                    </a>
-                </li2>
-            <?php } ?>
-
         </ul>
         <ul class="navbar-nav sidenav-toggler">
             <li class="nav-item">
@@ -112,27 +74,11 @@
             <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Crear">
                 <a class="nav-link" href="login.php">
                     <i class="fa fa-fw fa-sign-in"></i>
-                    <span class="nav-link-text">Por favor ingresar para poder usar el sistema.</span>
+                    <span class="nav-link-text">Por favor ingrese para poder usar el sistema.</span>
                 </a>
             </li>
-            <li2 class="nav-item" data-toggle="tooltip" data-placement="right" title="Crear">
-                <a class="nav-link">
-                  <p>
-                    <span class="nav-link-text">(?) Pantalla de Inicio: En esta pagina se muestra la pantalla inicial y el ingreso al sistema para estudiante, profesor y administrador.</span>
-                  </p>
-                  <span class="nav-link-text">- Profesor: puede crear, importar y buscar un OA en el repositorio.</span>
-                </br>
-                  <span class="nav-link-text">- Estudiante: puede buscar un OA en el repositorio.</span>
-                </br>
-                <span class="nav-link-text">- Administrador: maneja la información de los usuarios.</span>
-              </br>
-                </a>
-            </li2>
-            <button type= "button" class= "btn btn-danger" data-toggle="modal" data-target="#myModalIni" >
-            Ayuda
-            </button>
 
-  </ul>
+        </ul>
         <?php } ?>
 
         <ul class="navbar-nav ml-auto">
@@ -142,8 +88,7 @@
             <li class="nav-item">
                 <a class="nav-link" href="login.php">
                     <i class="fa fa-fw fa-sign-in"></i>Login</a>
-            </li<
-
+            </li>
             <?php } else { ?>
                 <li class="nav-item">
                 <a class="nav-link" href="userprof.php">
@@ -161,20 +106,9 @@
                     <a class="nav-link" data-toggle="modal" data-target="#exampleModal">
                         <i class="fa fa-fw fa-sign-out"></i>Logout</a>
                 </li>
-
-
             <?php } ?>
         </ul>
 
     </div>
 
 </nav>
-<div id="myModalIni" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" >
-  <div class="modal-dialog" >
-    <div class="modal-content"style="width: 1040px; height: 640px;">
-        <div class="modal-body">
-            <img src="AyudaIni.png" class="img-responsive" style="width: 1000px; height: 600px;">
-        </div>
-    </div>
-  </div>
-</div>
