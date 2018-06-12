9b241a54674027386cb8fbc2d17c9fffcbf6ba76
StevenCuasqui <cuasquisteven@gmail.com>
2018-06-12 14:53:04 -0500

    Commit sin Ayuda en línea

--- js/sb-admin.js
+++ js/sb-admin.js
@@ -43,28 +43,3 @@
     event.preventDefault();
   });
 })(jQuery); // End of use strict
-// Get the modal
-var modal = document.getElementById('myModal');
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = functionM() {
-    modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = functionM() {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = functionM(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
